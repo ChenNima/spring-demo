65240b633853df2a46022a67b2787e73dc8dc4e3
Felix Chen <fennu637@sina.com>
2020-06-19 19:25:01 +0800

    remove schema

--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE uers
-(
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    createdAt BIGINT NOT NULL
-);
